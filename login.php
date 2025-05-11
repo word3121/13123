57593a2baef16b4dc3e36a8ec32485d8807a48ae
dexyy <113134879+sxmn24@users.noreply.github.com>
2025-05-11 16:12:05 +0200

    Update login.php

--- login.php
+++ login.php
@@ -21,8 +21,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 } else {
 	// This is made by Magiczny_Jasiek, and only he can sell it. If you bought it from other vacban.wtf listing that was not this one: https://vacban.wtf/vacshop/78615/ then be careful using it. It is not official.
-    header("Location: index.php");
-    exit; 
+    $_SESSION['loggedin'] = true;
+        $_SESSION['username'] = $username;
+        
+        header("Location: dashboard.php");
+        exit;    } else {
+        echo "Invalid auth key.";
 }
 ?>
 
